7164cc729401a34c5e40713c3c597454004b99c7
an-rninn7 <jiangkenpingcheng@126.com>
2021-05-15 14:24:37 +0800

    init

---
+++ bookmall/src/com/demo/common/SessionInterceptor.java
@@ -0,0 +1,34 @@
+package com.demo.common;
+
+import com.jfinal.aop.Interceptor;
+import com.jfinal.aop.Invocation;
+import com.jfinal.core.Controller;
+
+/**
+ * 本 demo 仅表达最为粗浅的 jfinal 用法，更为有价值的实用的企业级用法 详见 JFinal 俱乐部:
+ * http://jfinal.com/club
+ * 
+ * BlogInterceptor 此拦截器仅做为示例展示，在本 demo 中并不需要
+ */
+public class SessionInterceptor implements Interceptor {
+
+	public void intercept(Invocation ai) {
+		System.out.println("SessionInterceptor Before invoking "
+				+ ai.getActionKey());
+
+		Controller ctl = ai.getController();
+
+		// 判断用户是否登录
+		// inv.getController().getSessionAttr(key)
+		String action = ai.getActionKey();
+		Object user = null;
+		if (user != null || action.indexOf("login") >= 0) {
+			ai.invoke();
+		} else {
+			// ctl.redirect("/login");
+			ai.invoke();
+		}
+		System.out.println("SessionInterceptor After invoking "
+				+ ai.getActionKey());
+	}
+}
